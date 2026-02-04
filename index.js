67e3f6d77acecf23dc45b8617cfe37fe10f9f97d
voltyea <volty764@gmail.com>
2026-02-04 13:36:41 +0530

    ...

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const video = document.getElementById('openingVideo');
 video.addEventListener('ended', () => {
-  video.classList.add('fade-out');
   document.body.classList.add('ready');
+  setTimeout(() => {
+    document.querySelector('.background').classList.add('unblurred');
+  }, 400);
 });
 
 video.addEventListener('transitionend', (e) => {
   if (e.propertyName === 'opacity') {
     video.remove();
+  }
+});
+
+video.addEventListener('timeupdate', () => {
+  if (video.currentTime >= video.duration - 0.10) {
+    video.classList.add('fade-out');
   }
 });
