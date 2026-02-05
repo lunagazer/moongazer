5417800c5846862ac99ea53fa92657b061f831cb
voltyea <volty764@gmail.com>
2026-02-05 12:43:24 +0530

    ...

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-const video = document.getElementById('openingVideo');
-video.addEventListener('ended', () => {
-  document.body.classList.add('ready');
+const video = document.getElementById("openingVideo");
+const popup = document.getElementById("popup");
+const closeBtn = document.getElementById("closePopup");
+
+popup.style.display = "flex";
+document.body.classList.add("popup-active");
+
+closeBtn.addEventListener("click", () => {
+  popup.style.display = "none";
+  document.body.classList.remove("popup-active");
+  video.play();
+});
+
+video.addEventListener("ended", () => {
+  document.body.classList.add("ready");
   setTimeout(() => {
-    document.querySelector('.background').classList.add('unblurred');
+    document.querySelector(".background").classList.add("unblurred");
   }, 400);
 });
 
-video.addEventListener('transitionend', (e) => {
-  if (e.propertyName === 'opacity') {
+video.addEventListener("transitionend", (e) => {
+  if (e.propertyName === "opacity") {
     video.remove();
   }
 });
 
-video.addEventListener('timeupdate', () => {
+video.addEventListener("timeupdate", () => {
   if (video.currentTime >= video.duration - 0.10) {
-    video.classList.add('fade-out');
+    video.classList.add("fade-out");
   }
 });
